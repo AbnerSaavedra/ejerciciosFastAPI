4c861ab444d48ca94c465ad9084ac7aaa62134cc
Abner Saavedra <ingenieroabnersaavedra@bitbucket.org>
2024-05-28 16:31:27 -0400

    Corrección manejo de autenticación por token

--- ejerciciosPracticos/seguridad/manejarToken.py
+++ ejerciciosPracticos/seguridad/manejarToken.py
@@ -21,14 +21,14 @@ dummy_users_db = {
    "johndoe": {
     "username": "johndoe",
     "email": "johndoe@example.com",
-    "hashed_password": "dumbhashedsecret",
+    "hashed_password": "secret",
     "full_name": "John Doe",
     "disabled": False
    },
    "alice": {
     "username": "alice",
     "email": "alice@example.com",
-    "hashed_password": "dumbhashedsecret2",
+    "hashed_password": "secret2",
     "full_name": "Alice Wonderson",
     "disabled": True
    }
@@ -70,6 +70,9 @@ def get_user(db, username: str):
         user_dict = db[username]
         return UserInDB(**user_dict)
 
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = dumb_decode_token(token)
     credentials_exception = HTTPException(
@@ -85,13 +88,10 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(dummy_users_db, username=token_data)
+    user = get_user(dummy_users_db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
-
-def dumb_hash_password(password: str):
-    return "dumbhashed" + password
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -100,7 +100,8 @@ def autheticate_user(fake_db, username: str, password: str):
     user = get_user(fake_db, username)
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    hashed_password = get_password_hash(user.hashed_password)
+    if not verify_password(password, hashed_password):
         return False
     return user
 
@@ -111,7 +112,6 @@ async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]
 @app.post("/token")
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
     user = autheticate_user(dummy_users_db, form_data.username, form_data.password)
-    user = dummy_users_db.get(form_data.username)
     if not user:
         raise HTTPException(
             status_code= status.HTTP_401_UNAUTHORIZED,
@@ -121,7 +121,6 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    
     return Token(access_token= access_token, token_type= "bearer")
 
 
